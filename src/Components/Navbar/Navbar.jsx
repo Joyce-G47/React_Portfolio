91c3db4a6c9486511ed8daae0a951983ea2744f0
Sibongile <sibongilegumbi47@gmail.com>
2024-02-07 13:20:38 +0200

    first commit

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <h1>S.Gumbi</h1>
+
+      <ul>
+        <li>Home</li>
+        <li>Skills</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  )
+}
+
+
+export default Navbar
